59ae696f5f587744dc1d35f42dddb8978a227131
tsamantanis <philip.tsamantanis@icloud.com>
2020-11-17 08:48:25 -0800

    Add grid to layout

--- src/components/DataStructures/DataStructure.js
+++ src/components/DataStructures/DataStructure.js
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
 import TitleCard from '../Helpers/TitleCard';
+import Grid from '../../assets/Grid.svg';
 class DataStructure extends Component {
     render() {
         return (
-            this.props.dataStructures.map((structure) => {
-                return (
-                    <div className="row mt-5">
-                        <div className="col-12 col-lg-4">
+            <div className="row mt-5">
+                <div className="col-12 col-lg-3">
+                    { this.props.dataStructures.map((structure) => {
+                        return (
                             <TitleCard
                                 title={structure.title}
                                 color={structure.color}
                             />
-                        </div>
-                        <div className="col-12 col-lg-8 text-white text-center">
-                            GRID
-                        </div>
+                        );
+                    })}
                     </div>
-                );
-            })
-
+                <div className="col-12 col-lg-8 offset-lg-1 text-white text-center">
+                    <img src={Grid} className="img-fluid" alt="Diagram Grid"/>
+                </div>
+            </div>
         );
     }
 }
