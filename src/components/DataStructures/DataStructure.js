aad9847e1c40625761b4acf39fa7d4fe7f80bc85
tsamantanis <philip.tsamantanis@icloud.com>
2020-11-17 07:56:49 -0800

    Initial setup

---
+++ src/components/DataStructures/DataStructure.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class DataStructure extends Component {
+    render() {
+        return (
+            <>{/*<TitleCard
+                title="ARRAY"
+                color="turquoise"
+            />*/}
+            </>
+        );
+    }
+}
+
+export default DataStructure;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import DataStructure from '../DataStructures/DataStructure';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <DataStructure />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
