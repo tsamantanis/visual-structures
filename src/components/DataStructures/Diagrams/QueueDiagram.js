e6bac4dffc0f76f59765d2f6ffc1cb6bf90b0165
tsamantanis <philip.tsamantanis@icloud.com>
2020-12-13 09:29:30 -0800

    Improved queue svg & animation

--- src/components/DataStructures/Diagrams/QueueDiagram.js
+++ src/components/DataStructures/Diagrams/QueueDiagram.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import squares from "../../Helpers/squares";
 import QueueLinkedList from "../Queue/QueueLinkedList.js";
+import QueueArraySVG from '../../../assets/Queue Array Visual.svg';
 
 const list = new QueueLinkedList();
 list.enqueue(1);
@@ -15,7 +16,13 @@ class QueueDiagram extends Component {
     }
 
     render() {
-        return this.state.list.displayList();
+        return (
+            <div className="row justify-content-center align-items-center">
+                <div className="col-12 col-lg-8">
+                    <img className="img-fluid" src={ QueueArraySVG } alt="Queue enqueue and dequeue method SVG animation" />
+                </div>
+            </div>
+        )
     }
 }
 
