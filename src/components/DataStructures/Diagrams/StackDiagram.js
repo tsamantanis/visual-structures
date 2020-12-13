5f7a77c5aec5d6911780aed22c158bb3f91e7824
tsamantanis <philip.tsamantanis@icloud.com>
2020-12-13 09:13:27 -0800

    Improved stack svg & animation

--- src/components/DataStructures/Diagrams/StackDiagram.js
+++ src/components/DataStructures/Diagrams/StackDiagram.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import squares from "../../Helpers/squares";
 import StackLinkedList from "../Stack/StackLinkedList.js";
+import StackArraySVG from '../../../assets/Stack Array Visual.svg';
 
 const list = new StackLinkedList();
 list.push(1);
@@ -15,7 +16,13 @@ class StackDiagram extends Component {
     }
 
     render() {
-        return this.state.list.displayList();
+        return (
+            <div className="row justify-content-center align-items-center">
+                <div className="col-12 col-lg-8">
+                    <img className="img-fluid" src={ StackArraySVG } alt="Stack push and pop method SVG animation" />
+                </div>
+            </div>
+        )
     }
 }
 
